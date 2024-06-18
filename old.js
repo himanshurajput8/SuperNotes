8fd6d59bd36a52dbdb32605320e8c4aed50bd63c
himanshurajput8 <rajputhimanshu913@gmail.com>
2024-06-18 11:16:46 +0530

    fixed

--- old.js
+++ old.js
@@ -17,15 +17,16 @@ var jobDetails = JSON.parse(localStorage.getItem('jobDetails')) || [
   { id: "6", date: '01 June 2024', title: 'Job interview preparations', description: 'Your success in a job interview depends a great deal on how you prepare for it. Interview preparation primarily involves researching the job and the company and thoughtfully considering your answers to the interview question' },
 
   { id: "7", date: '01 July 2024', title: 'Job interview preparations', description: 'Your success in a job interview depends a great deal on how you prepare for it. Interview preparation primarily involves researching the job and the company and thoughtfully considering your answers to the interview question' }
+
 ]
-var container = document.createElement('div') 
+var container = document.createElement('div')
 container.className = "container"
 
 var leftDiv = document.createElement('div')//leftdiv  
 leftDiv.className = "left-div"
 container.appendChild(leftDiv)
 
-var centerDiv = document.createElement('div')//centerdiv  
+var centerDiv = document.createElement('div')//centerdiv
 centerDiv.className = "center-div"
 container.appendChild(centerDiv)
 
@@ -54,6 +55,9 @@ shareButton.innerHTML = '<i class="fa-solid fa-share-nodes"></i>'
 leftDiv.appendChild(shareButton)
 
 document.body.appendChild(container)
+
+
+
 // renderdata
 function renderData() {
   var content = jobDetails.map(function (item) {
@@ -72,10 +76,26 @@ function renderData() {
   deleteButtons.forEach(function (button) {
     button.addEventListener('click', function (event) {
       var id = event.target.getAttribute('data-id');
-      deleteItem(id)    
+      deleteItem(id)
     });
   });
-  //function delete innerDiv
+
+  // Add event listeners to innerDivs
+  var innerDivs = document.querySelectorAll('.inner-div');
+  innerDivs.forEach(function (item) {
+    item.addEventListener('click', function () {
+      var id = item.getAttribute('data-id')
+      document.querySelectorAll('.inner-div').forEach(function (note) {
+        note.classList.remove('active');
+        note.style.backgroundColor = '';
+      });
+      item.classList.add('active');
+      item.style.backgroundColor = 'red';
+      renderDataRightDiv(jobDetails, index); 
+    });
+  });
+}
+//function delete innerDiv
 function deleteItem(id) {
   jobDetails = jobDetails.filter(function (item) {
     return item.id !== id;
@@ -84,28 +104,6 @@ function deleteItem(id) {
   renderData()
 }
 
-  // Add event listeners to innerDivs
-  var innerDivs = document.querySelectorAll('.inner-div');
-  innerDivs.forEach(function (item) {     
-    item.addEventListener('click', function (){
-      var id = item.getAttribute('data-id') 
-      console.log(id)
-      var jobDetailsObject = jobDetails.find(function (item1) {
-        return item1.id === id
-      })
-      console.log(jobDetailsObject) 
-      document.querySelectorAll('.inner-div').forEach(function (note) {
-        note.classList.remove('active');
-        note.style.backgroundColor = '';
-      });
-      item.classList.add('active');
-      item.style.backgroundColor = 'rgb(146, 65, 252)';  
-      renderDataRightDiv(jobDetailsObject);
-    });
-  });    
-}
-
-
 //sort by date
 jobDetails.sort(function (a, b) {
   return new Date(b.date) - new Date(a.date);
@@ -113,7 +111,8 @@ jobDetails.sort(function (a, b) {
 renderData()
 
 // render data in rightDiv 
-function renderDataRightDiv(jobDetailsObject) {
+function renderDataRightDiv(jobDetails, index) {
+  var dataindex = rightDiv.setAttribute('data-index', index)
   rightDiv.innerHTML = ""
 
   var headingdiv = document.createElement('h3')
@@ -123,27 +122,16 @@ function renderDataRightDiv(jobDetailsObject) {
   descriptionDiv.id = "content"
 
   titleDiv.contentEditable = "true"
-  descriptionDiv.contentEditable = "true"  
+  descriptionDiv.contentEditable = "true"
 
-  headingdiv.textContent = jobDetailsObject.date
-  titleDiv.textContent = jobDetailsObject.title
-  descriptionDiv.innerHTML = jobDetailsObject.description
+  headingdiv.textContent = jobDetails[index].date
+  titleDiv.textContent = jobDetails[index].title
+  descriptionDiv.innerHTML = jobDetails[index].description
 
   rightDiv.appendChild(headingdiv)
   rightDiv.appendChild(titleDiv)
-  
-  // addEventlistener to innerDiv
-  titleDiv.addEventListener('input', function(){
-    jobDetailsObject.title = titleDiv.textContent;
-    jobDetailsObject.date = new Date().toLocaleDateString('en-US', { day: 'numeric', month: 'short', year: 'numeric' });
 
-    saveToLocalStorage();
-    renderData();
-    
-  })
-   
-
-  if (jobDetailsObject.type === 'drawing') {
+  if (jobDetails[index].type === 'drawing') {
     var canvasDiv = document.createElement('div')
     canvasDiv.className = "canvas-div"
     canvasDiv.innerHTML = '<canvas id="canvas"></canvas>'
@@ -153,40 +141,40 @@ function renderDataRightDiv(jobDetailsObject) {
       isDrawingMode: true
     })
     // canvas autoSave
-    if (jobDetailsObject.description.includes('<img')) {
-      var imgSrc = jobDetailsObject.description.match(/src="([^"]+)"/)[1];
+    if (jobDetails[index].description.includes('<img')) {
+      var imgSrc = jobDetails[index].description.match(/src="([^"]+)"/)[1];
       fabric.Image.fromURL(imgSrc, function (img) {
         canvas.setBackgroundImage(img, canvas.renderAll.bind(canvas));
       });
     }
     canvas.on('object:modified', function () {
-      autoSaveDrawing(canvas, jobDetailsObject);
+      autoSaveDrawing(canvas);
     });
     canvas.on('path:created', function () {
-      autoSaveDrawing(canvas, jobDetailsObject);
+      autoSaveDrawing(canvas);
     });
-
     //canvas autoSave function
     function autoSaveDrawing(canvas) {
       var drawingData = canvas.toDataURL('image/png');
-      jobDetailsObject.description = `<img src="${drawingData}" />`;
+      jobDetails[index].description = `<img src="${drawingData}" />`;
       saveToLocalStorage();
       renderData(jobDetails);
-      renderDataRightDiv(jobDetailsObject)
+      renderDataRightDiv(jobDetails, index)
     }
     // clear Canvas Button
     var clearButton = document.createElement('button');
     clearButton.textContent = 'Clear Canvas';
     clearButton.classList.add('canvas-clear-btn');
     rightDiv.appendChild(clearButton);
+    var index = rightDiv.getAttribute('data-index');
 
     // add addEventListener to clear canvas button
     clearButton.addEventListener('click', function () {
       canvas.clear();
-      jobDetailsObject.description = '';
+      jobDetails[index].description = '';
       saveToLocalStorage();
       renderData()
-      renderDataRightDiv(jobDetailsobject);
+      renderDataRightDiv(jobDetails, index);
     });
 
   } else {
@@ -194,111 +182,90 @@ function renderDataRightDiv(jobDetailsObject) {
     var quillContainer = document.createElement('div')
     quillContainer.className = "content"
     rightDiv.appendChild(quillContainer)
-    quillContainer.textContent = jobDetailsObject.date
-    quillContainer.textContent = jobDetailsObject.title
-    quillContainer.innerHTML = jobDetailsObject.description
-    var quill = new Quill(".content", { 
+    quillContainer.textContent = jobDetails[index].date
+    quillContainer.textContent = jobDetails[index].title
+    quillContainer.innerHTML = jobDetails[index].description
+    var quill = new Quill(".content", {
       theme: "snow"
     })
     quill.on('text-change', function () {
-      jobDetailsObject.description = quill.root.innerHTML;
-      jobDetailsObject.date = new Date().toLocaleDateString('en-US', { day: 'numeric', month: 'short', year: 'numeric' });
+      jobDetails[index].description = quill.root.innerHTML;
+      jobDetails[index].date = new Date().toLocaleDateString('en-US', { day: 'numeric', month: 'short', year: 'numeric' });
 
       saveToLocalStorage()
-      renderData()
+      renderData(jobDetails)
     });
   }
-  
-  rightDiv.addEventListener('input', function () {
-    changeData(jobDetailsObject)
-  })
-  
+  rightDiv.addEventListener('input', changeData)
 }
 //change data when edit
-function changeData(jobDetailsObject) {
+function changeData() {
+  var index = rightDiv.getAttribute('data-index')
   var headingdiv = rightDiv.querySelector('h3')
-  var descriptionDiv = rightDiv.querySelector('p')     
-  saveToLocalStorage() 
-  renderData()
-  var changeDate = document.querySelector('.heading-div') 
-  headingdiv.textContent = jobDetailsObject.date 
-}
+  var titleDiv = rightDiv.querySelector('h4')
+  var descriptionDiv = rightDiv.querySelector('p')
 
+  jobDetails[index].date = new Date().toLocaleDateString('en-US', { day: 'numeric', month: 'short', year: 'numeric' });
+  jobDetails[index].title = titleDiv.textContent
+  jobDetails[index].description = descriptionDiv.textContent
+  saveToLocalStorage()
+  renderData()
+  var changeDate = document.querySelector('.heading-div')
+  changeDate.textContent = jobDetails[index].date
+}
 // addButton
 addButton1.addEventListener('click', function () {
   var date = new Date().toLocaleDateString('en-US', { day: 'numeric', month: 'short', year: 'numeric' });
-  var newObject = { id: String(jobDetails.length + 1), date: date, title: 'Title..', description: 'Description...' }
+  var newObject = { date: date, title: 'Title..', description: 'Description...' }
   jobDetails.unshift(newObject)
   saveToLocalStorage()
   renderData()
-  renderDataRightDiv(newObject) 
+  renderDataRightDiv(jobDetails, 0)
 })
 
 // task button
 taskButton.addEventListener('click', function () {
   var date = new Date().toLocaleDateString('en-US', { day: 'numeric', month: 'short', year: 'numeric' });
-  var newObject = { id: String(jobDetails.length + 1), date: date, title: 'Title..', description: 'to do: <ul><li></li></ul>' }
+  var newObject = { date: date, title: 'Title..', description: 'to do: <ul><li></li></ul>' }
   jobDetails.unshift(newObject)
   saveToLocalStorage()
   renderData()
-  renderDataRightDiv(newObject)
+  renderDataRightDiv(jobDetails, 0)
 })
-renderDataRightDiv(jobDetails[0]);
+renderDataRightDiv(jobDetails, 0)
 
 // scrible button function
 function scriblebtn() {
   rightDiv.innerHTML = ""
   var date = new Date().toLocaleDateString('en-US', { day: 'numeric', month: 'short', year: 'numeric' });
-  var newObject = { id: String(jobDetails.length + 1), date: date, title: 'Title..', description: 'Description...', type: 'drawing' }
+  var newObject = { date: date, title: 'Title..', description: 'Description...', type: 'drawing' }
   jobDetails.unshift(newObject)
   saveToLocalStorage()
   renderData()
-  renderDataRightDiv(newObject)
+  renderDataRightDiv(jobDetails, 0)
 }
 scribleButton.addEventListener('click', scriblebtn)
 
 // localStorage function
 function saveToLocalStorage() {
-  localStorage.setItem('jobDetails', JSON.stringify(jobDetails)); 
+  localStorage.setItem('jobDetails', JSON.stringify(jobDetails));
 }
-
-// Add event listener to shareButton
+// add addEventListener to shareButton
 shareButton.addEventListener('click', async function () {
-  try {    
-    var activeInnerDiv = document.querySelector('.inner-div');
-    if (!activeInnerDiv) {
-      console.error('No active item found to share.');
-      return;
-    }
-
-    var id = activeInnerDiv.getAttribute('data-id');
-    var jobDetailsObject = jobDetails.find(function (item) {
-      return item.id === id;
-    });
-
-    if (!jobDetailsObject) {
-      console.error('No job details object found with id:', id);
-      return;
-    }
-
+  try {
+    var index = rightDiv.getAttribute('data-index');
     if (navigator.share) {
       await navigator.share({
-        title: jobDetailsObject.title,
-        text: jobDetailsObject.description,
+        title: jobDetails[index].title,
+        text: jobDetails[index].description,
         url: window.location.href
       });
     } else {
       console.log('Web Share API not supported.');
     }
   } catch (error) {
-    console.error('Error sharing:', error);
+    console.error('Error sharing:', error); 
   }
 });
-
-
-
-
-
-
 
 
